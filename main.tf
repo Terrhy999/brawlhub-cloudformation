69024c4ff131c2015ee4735e839f550fff5b357e
Terrhy999 <thyla999@gmail.com>
2024-11-25 13:48:28 -0500

    terraform brawlhub vpc

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+# Create a VPC for Brawlhub
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+    tags = {
+    Name = "brawlhub-terraform"
+  }
+}
